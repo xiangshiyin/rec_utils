78b2e12c2f609ce32f22a0fc4bcbf6a530134315
Xiangshi Yin <xiangshi.yin@coxautoinc.com>
2020-03-22 12:31:42 -0400

    finished ncf enhancement test

--- cf_ec2/ncf.py
+++ cf_ec2/ncf.py
@@ -19,7 +19,12 @@ from keras.layers import (
     Concatenate,
     Dense
 )
-from . import gmf, mlp, dataPrep
+from . import (
+    gmf, 
+    mlp, 
+    dataPrep,
+    evaluation_grouped
+)
 
 
 class NCF:
@@ -40,7 +45,7 @@ class NCF:
         self.reg_gmf = reg_gmf
         self.reg_layers_mlp = reg_layers_mlp # [0,0,0,0]
 
-    def create_model(self):
+    def create_model(self, path_pretrain=None):
         num_layers_mlp = len(self.layers_mlp)
         ## create the input layer
         self.users_input = Input(shape=(1,), dtype='int32', name='user_input')
@@ -114,26 +119,30 @@ class NCF:
             inputs=[self.users_input,self.items_input], 
             outputs=prediction
         )
-        return model
+        
+        ## load pretrain ncf model
+        if path_pretrain:
+            model.load_weights(path_pretrain)
+        self.model = model
     
-    def load_pretrain_model(self, model, model_gmf, model_mlp, num_layers_mlp):
+    def load_pretrain_model(self, model_gmf, model_mlp, num_layers_mlp):
         ## get the embedding weights
         #### GMF embedding branch
         w_embedding_gmf_User = model_gmf.get_layer('embedding_gmf_User').get_weights()
         w_embedding_gmf_Item = model_gmf.get_layer('embedding_gmf_Item').get_weights()
-        model.get_layer('embedding_gmf_User').set_weights(w_embedding_gmf_User)
-        model.get_layer('embedding_mlp_Item').set_weights(w_embedding_gmf_Item)
+        self.model.get_layer('embedding_gmf_User').set_weights(w_embedding_gmf_User)
+        self.model.get_layer('embedding_mlp_Item').set_weights(w_embedding_gmf_Item)
         #### MLP embedding branch
         w_embedding_mlp_User = model_mlp.get_layer('embedding_mlp_User').get_weights()
         w_embedding_mlp_Item = model_mlp.get_layer('embedding_mlp_Item').get_weights()
-        model.get_layer('embedding_mlp_User').set_weights(w_embedding_mlp_User)
-        model.get_layer('embedding_mlp_Item').set_weights(w_embedding_mlp_Item)
+        self.model.get_layer('embedding_mlp_User').set_weights(w_embedding_mlp_User)
+        self.model.get_layer('embedding_mlp_Item').set_weights(w_embedding_mlp_Item)
 
         #### the MLP layers
         for idx in range(1,num_layers_mlp):
             name_mlp_layer = 'mlp_layer_{}'.format(idx)
             w_mlp_layer = model_mlp.get_layer(name_mlp_layer).get_weights()
-            model.get_layer(name_mlp_layer).set_weights(w_mlp_layer)
+            self.model.get_layer(name_mlp_layer).set_weights(w_mlp_layer)
         
         #### the output layer
         w_gmf_output = model_gmf.get_layer('output').get_weights()
@@ -143,132 +152,59 @@ class NCF:
             axis=0
         )
         b_ncf_output = 0.5*(w_gmf_output[1]+w_mlp_output[1])
-        model.get_layer('output').set_weights([
+        self.model.get_layer('output').set_weights([
             w_ncf_output,
             b_ncf_output
         ])
-        return model
-
-
-
-
-
-
-def get_train():
-    return '','',''
-
-#### train the NCF model
-def etl():
-    pass
-    ## step 1: load the data
-    n_users = 10000
-    n_items = 50000
-    n_factors_gmf = 30
-    layers_mlp = [64,32,16,8]
-    reg_gmf = 0.
-    reg_layers_mlp = [0.,0.,0.,0.]
-    learning_rate = 0.001
-    flg_pretrain = ''
-    filepath = ''
-    filepath_gmf_pretrain = ''
-    filepath_mlp_pretrain = ''
-    num_epochs = 5
-    batch_size = 5
-
-    ## step 2: build the model
-    ncf = NCF(
-        n_users=n_users,
-        n_items=n_items,
-        n_factors_gmf=n_factors_gmf,
-        layers_mlp=layers_mlp,
-        reg_gmf=reg_gmf,
-        reg_layers_mlp=reg_layers_mlp
-    )
-    model = ncf.create_model()
-    #### compile the model
-    model.compile(
-        optimizer=Adam(lr=learning_rate),
-        loss='binary_crossentropy'
-    )
-
-    ## step 3: load pretrained model
-    if filepath_gmf_pretrain!='' and filepath_mlp_pretrain!='':
-        #### initialize the models
-        model_gmf = gmf.GMF(
-            n_users=n_users,
-            n_items=n_items,
-            n_factors_gmf=n_factors_gmf,
-            reg_gmf=reg_gmf
-        ).create_model()
-        model_mlp = mlp.MLP(
-            n_users=n_users,
-            n_items=n_items,
-            layers_mlp=layers_mlp,
-            reg_layers_mlp=reg_layers_mlp
-        ).create_model()
-        #### load the pretrained weights
-        model_gmf.load_weights(filepath_gmf_pretrain)
-        model_mlp.load_weights(filepath_mlp_pretrain)
-        #### combine and generate the full ncf model
-        model = ncf.load_pretrain_model(
-            model=model,
-            model_gmf=model_gmf,
-            model_mlp=model_mlp,
-            num_layers_mlp=len(layers_mlp)
+    
+    def compile(self,learning_rate):
+        ## compile the model
+        self.model.compile(
+            optimizer=Adam(learning_rate=learning_rate),
+            loss='binary_crossentropy',
+            metrics=['accuracy']
         )
 
+    def fit(self, dataset, batch_size, num_epochs, path_model_weights, path_csvlog):
+        ## create the callback metrics
+        checkpoint = keras.callbacks.ModelCheckpoint(
+            filepath= path_model_weights, 
+            verbose=1, 
+            save_best_only=True
+        )
+        csvlog = keras.callbacks.CSVLogger(
+            filename=path_csvlog, 
+            separator=',', 
+            append=False
+        )
+        earlystop = keras.callbacks.EarlyStopping(patience=12)
+        lrreduce = keras.callbacks.ReduceLROnPlateau(
+            monitor="val_loss", 
+            factor=0.3, 
+            patience=4, 
+            verbose=1
+        )  
+        metrics2 = evaluation_grouped.metricsCallback()      
+        ## fit the model
+        hist = self.model.fit(
+            x = [
+                np.array(dataset.users),
+                np.array(dataset.items)
+            ],
+            y = np.array(dataset.ratings),
+            batch_size=batch_size,
+            epochs=num_epochs,
+            verbose=2,
+            shuffle=True,
+            callbacks=[metrics2,checkpoint,csvlog,earlystop,lrreduce],
+            validation_data=(
+                [
+                    np.array(dataset.users_test),
+                    np.array(dataset.items_test)
+                ],
+                np.array(dataset.ratings_test)
+            )
+        )  
+        return hist
 
 
-    ## step 4: train the model
-    #### load the raw training data
-    train = pd.read_csv(
-        '',
-        sep=',',
-        header=0
-    )
-    #### generate the set of user/item/label lists
-    dataset = dataPrep.Data(
-        train=train,
-        col_user='contact_uid',
-        col_item='item',
-        col_rating='freq',
-        binary=True
-    )
-    dataset.prepTrainDNN()
-    users_input = dataset.users
-    items_input = dataset.items
-    labels = dataset.ratings
-
-
-    #### train
-    hist = model.fit(
-        x = [
-            np.array(users_input),
-            np.array(items_input)
-        ],
-        y = np.array(labels),
-        batch_size=batch_size,
-        epochs=1,
-        verbose=2,
-        shuffle=True
-    )
-    # verbose: Integer. 0, 1, or 2. Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch.
-
-
-    ## step 5: save the model weights
-    model.save_weights(filepath=filepath) # saves weights of the model as HDF5 file
-    # model.load_weights(filepath=filepath, by_name=False) # load the pretrained weights
-
-
-
-
-if __name__ == "__main__":
-    etl()
-
-
-
-
-
-
-
-    
